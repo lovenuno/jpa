9e7c40197c39fb6d9d77493a589888145d52048f
love NUNO <nuno08@gmail.com>
2022-11-25 12:03:34 +0900

    1차 완성

---
+++ src/main/java/com/springboot/jpa/data/dao/impl/ProductDAOImpl.java
@@ -0,0 +1,68 @@
+package com.springboot.jpa.data.dao.impl;
+
+import com.springboot.jpa.data.dao.ProductDAO;
+import com.springboot.jpa.data.entity.Product;
+import com.springboot.jpa.data.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Component
+public class ProductDAOImpl implements ProductDAO {
+
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductDAOImpl(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+
+    @Override
+    public Product insertProduct(Product product) {
+
+        Product saveProduct = productRepository.save(product);
+        return saveProduct;
+    }
+
+    @Override
+    public Product selectProduct(Long number) {
+//        Product selectedProduct = productRepository.getById(number);
+        Product selectedProduct = productRepository.findById(number).get();
+        return selectedProduct;
+    }
+
+    @Override
+    public Product updateProductName(Long number, String name) throws Exception {
+        Optional<Product> selectedProduct = productRepository.findById(number);
+
+        Product updatedProduct;
+
+        if(selectedProduct.isPresent()){
+            Product product = selectedProduct.get();
+
+            product.setName(name);
+            product.setUpdateAt(LocalDateTime.now());
+
+            updatedProduct = productRepository.save(product);
+        } else {
+            throw new Exception();
+        }
+        return updatedProduct;
+    }
+
+    @Override
+    public void deleteProduct(Long number) throws Exception {
+        Optional<Product> selectedProduct = productRepository.findById(number);
+
+        if(selectedProduct.isPresent()){
+            Product product = selectedProduct.get();
+
+            productRepository.delete(product);
+        } else {
+            throw new Exception();
+        }
+    }
+}
